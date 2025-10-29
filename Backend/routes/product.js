c3f3831cdf32594fadf21a1f4b54ebc34149106e
Vrushabh Bhotmange <vrushabhbhotmange01@gmail.com>
2025-10-29 19:37:57 +0530

    change

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/",protect,authorize("admin"), createProduct);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
+router.put("/:id", protect,authorize("admin"),updateProduct);
 router.delete("/:id",protect,authorize("admin"), deleteProduct);
 
 export default router;
